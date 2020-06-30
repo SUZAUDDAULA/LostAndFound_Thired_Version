a9135f43ba9644b27048cf7b957a50a7258e29d8
suzauddaula <suzauddaula103@gmail.com>
2020-06-30 10:00:25 +0600

    likeongoing4

--- app/src/main/java/com/opus_bd/lostandfound/Adapter/NewsFeedAdapter.java
+++ app/src/main/java/com/opus_bd/lostandfound/Adapter/NewsFeedAdapter.java
@@ -118,7 +118,6 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.Transa
                 tvtime.setText(item.getGdDate());
                 tvStatus.setText(item.getVehicleDescription());
                 iv_Sub_pic.setImageBitmap(decodedImage);
-                liketext.setId(item.getVehicleId());
                 totalLike.setText(String.valueOf(item.getTotalLikes()));
                 //Glide.with(context).load("http://103.134.88.13:1022/"+item.getAttachImage()).into(iv_Sub_pic);
                 Glide.with(context).load("http://103.134.88.13:1022/"+item.getProfilePic()).into(user_prifile_pic);
