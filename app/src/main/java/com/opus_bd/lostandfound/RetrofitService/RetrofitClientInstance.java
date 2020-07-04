2fbe138253125711e89eb3eefe3c1d2c3bacec49
suzauddaula <suzauddaula103@gmail.com>
2020-07-04 10:31:14 +0600

    fullimageview2

--- app/src/main/java/com/opus_bd/lostandfound/RetrofitService/RetrofitClientInstance.java
+++ app/src/main/java/com/opus_bd/lostandfound/RetrofitService/RetrofitClientInstance.java
@@ -11,7 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
     //private static final String BASE_URL = "http://103.134.88.13:1022/";
-    private static final String BASE_URL = "http://d9bd803aff54.ngrok.io/";
+    private static final String BASE_URL = "http://7f878d7256df.ngrok.io/";
     //private static final String BASE_URL = "http://127.0.0.1:5099/";
     private static Retrofit retrofit;
     private static Gson gson = new GsonBuilder()
